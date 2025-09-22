1b673d672396b60f6ffae95f6e526a8e5faea2fe
blu14x <blu14x@gmail.com>
2025-09-22 19:52:24 +0200

    chore: prepare semantic-release

--- src/datachoices/__init__.py
+++ src/datachoices/__init__.py
@@ -1,4 +1,5 @@
 from .choices import DataChoices
 from .fields import DataChoiceArrayField, DataChoiceField
 
+__version__ = '0.0.2'
 __all__ = ('DataChoiceArrayField', 'DataChoiceField', 'DataChoices')
