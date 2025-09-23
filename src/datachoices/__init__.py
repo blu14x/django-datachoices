877c0bb18b6c18751429e82c3f737979b111ff13
blu14x <blu14x@gmail.com>
2025-09-23 10:59:56 +0200

    chore: cleanup

--- src/datachoices/__init__.py
+++ src/datachoices/__init__.py
@@ -1,5 +1,5 @@
 from .choices import DataChoices
 from .fields import DataChoiceArrayField, DataChoiceField
 
-__version__ = '0.0.2'
+__version__ = '0.1.1'
 __all__ = ('DataChoiceArrayField', 'DataChoiceField', 'DataChoices')
